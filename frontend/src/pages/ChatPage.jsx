09977af6d53e7a4ed4e045aa553864a8f1cd6b2a
VanHadsoN <107035951+VanHadsoN@users.noreply.github.com>
2023-09-18 13:20:27 +0300

    Step 2. Updated "frontend\src\pages\ChatPage.jsx"

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,44 +1,73 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useRollbar } from '@rollbar/react';
+import { useAuthorization, useChatApi } from '../hooks';
+import fetchInitialData from '../context/InitialDataThunk';
+import { messagesSelector, currentChannel } from '../selectors';
+import ChannelsPanel from '../components/chat/channels-panel/ChannelsPanel';
+import ChatPanel from '../components/chat/chat-panel/ChatPanel';
+import ModalWindow from '../components/modal/ModalWindow';
+import MessageBox from '../components/chat/message-box/MessageBox';
+import MessageForm from '../components/chat/MessageForm';
+import { appRoutes } from '../routes';
 
-import fetchData from '../actions/fetchData.js';
-
-import Channels from '../components/Channels.jsx';
-import Messages from '../components/Messages.jsx';
-import showToast from '../toastify/showToast.js';
-
-const Chat = () => (
-    <div className="container h-100 my-4 overflow-hidden rounded shadow">
-      <div className="row h-100 bg-white flex-md-row">
-        <Channels />
-        <Messages />
-      </div>
-    </div>
-);
-
-const ChatPage = () => {
-    const dispatch = useDispatch();
-    const { t } = useTranslation();
+const Chat = () => {
+  const { t } = useTranslation();
+  const rollbar = useRollbar();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { getServerData } = useChatApi();
+  const { logOut } = useAuthorization();
+  const messages = useSelector(messagesSelector.selectAll);
+  const currentChannelData = useSelector(currentChannel);
+  const currentChannelName = currentChannelData?.name;
+  const currentChannelMessages = messages.filter(
+    (message) => message.сhannelId === currentChannelData?.id,
+  );
+  const currentChannelMessagesCount = currentChannelMessages.length;
+  const loadingStatus = useSelector((state) => state?.loading?.serverData);
 
   useEffect(() => {
-    const handleFetchData = async () => {
-      try {
-        await dispatch(fetchData());
-      } catch (error) {
-        if (error.isAxiosError && error.response.status === 409) {
-          showToast(t('toastify.connectionError'), 'warning');
-        } else {
-          showToast(t('toastify.athorizationError'), 'error');
-          console.error(error.message);
-        }
-      }
-    };
+    dispatch(fetchInitialData(getServerData));
+  }, [dispatch, getServerData]);
 
-    handleFetchData();
-  }, [dispatch, t]);
+  useEffect(() => {
+    if (loadingStatus === 'failed') {
+      logOut();
+      navigate(appRoutes.loginPagePath());
+      toast.error(t('toast.networkError'));
+      rollbar.error('ChatFailed');
+    }
 
-  return (<Chat />);
+    if (loadingStatus === 'authError') {
+      logOut();
+      navigate(appRoutes.loginPagePath());
+      toast.error(t('toast.authError'));
+      rollbar.error('AuthFailed');
+    }
+  }, [loadingStatus, logOut, navigate, rollbar, t]);
+
+  return (
+    <div className="container h-100 my-4 overflow-hidden rounded shadow">
+      <div className="row h-100 bg-white flex-md-row">
+        <ModalWindow />
+        <ChannelsPanel />
+        <div className="col p-0 h-100">
+          <div className="d-flex flex-column h-100">
+            <ChatPanel
+              currentChannelName={currentChannelName}
+              currentChannelMessagesCount={currentChannelMessagesCount}
+            />
+            <MessageBox currentChannelMessages={currentChannelMessages} />
+            <MessageForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default ChatPage;
+export default Chat;
