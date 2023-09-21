060f5f7e28d80e3a01183a535cc9a3229ffd8c4d
VanHadsoN <107035951+VanHadsoN@users.noreply.github.com>
2023-09-21 12:54:37 +0300

    Step 3. Created "frontend\src\context\ChatContext.js"

---
+++ frontend/src/context/ChatContext.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import { createContext } from 'react';
+import { useDispatch } from 'react-redux';
+import { addChannel, setCurrentChannel } from '../slices/channelsSlice';
+import routes  from '..routes.js';
+
+export const ChatContext = createContext({});
+
+const ChatContextProvider = ({ socket, children }) => {
+  const dispatch = useDispatch();
+  const timeout = 4000;
+
+  const addNewMessage = async (message) => {
+    await socket
+      .timeout(timeout)
+      .emit('newMessage', message);
+  };
+
+  const addNewChannel = async (channel) => {
+    const { data } = await socket
+      .timeout(timeout)
+      .emitWithAck('newChannel', channel);
+
+    dispatch(addChannel(data));
+    dispatch(setCurrentChannel(data.id));
+  };
+
+  const removeSelectedChannel = async (id) => {
+    await socket
+      .timeout(timeout)
+      .emit('removeChannel', { id });
+  };
+
+  const renameSelectedChannel = async (updateChannel) => {
+    await socket
+      .timeout(timeout)
+      .emit('renameChannel', updateChannel);
+  };
+
+  const getServerData = async () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const response = await axios.get(routes.data(), { headers: { Authorization: `Bearer ${user.token}` } });
+    return response;
+  };
+
+  return (
+    <ChatContext.Provider value={{
+      addNewMessage,
+      addNewChannel,
+      removeSelectedChannel,
+      renameSelectedChannel,
+      getServerData,
+    }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export default ChatContextProvider;
