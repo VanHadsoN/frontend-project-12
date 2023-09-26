89ee34a04697dd3b8625cb0af3488b2f89ed056e
VanHadsoN <107035951+VanHadsoN@users.noreply.github.com>
2023-09-26 13:13:55 +0300

    Step 3. Created "frontend\src\components\login\LoginFooter.jsx"

---
+++ frontend/src/components/login/LoginFooter.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import routes  from '..routes.js';
+
+const LoginFooter = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="card-footer p-4">
+      <div className="text-center">
+        <span className="no-account">
+          {t('login.noAccount')}
+          {' '}
+        </span>
+        <NavLink to={routes.signupPagePath()} className="link">
+          {t('registration.registrationTitle')}
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default LoginFooter;
