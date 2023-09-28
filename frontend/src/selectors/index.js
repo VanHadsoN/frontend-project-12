58c45d8824050c77002da7260db72ce68405ae73
VanHadsoN <107035951+VanHadsoN@users.noreply.github.com>
2023-09-28 12:17:50 +0300

    Step 4. Created "frontend\src\selectors\index.js"

---
+++ frontend/src/selectors/index.js
@@ -0,0 +1,20 @@
+import { messagesAdapter } from '../slices/messagesSlice';
+import { channelsAdapter } from '../slices/channelsSlice';
+import { modalWindowAdapter } from '../slices/modalWindowSlice';
+
+const channelsSelector = channelsAdapter.getSelectors((state) => state.channels);
+const currentChannel = (state) => (
+  channelsSelector.selectById(state, state.channels.currentChannelId));
+const channelsNames = (state) => channelsSelector.selectAll(state).map((channel) => channel.name);
+
+const messagesSelector = messagesAdapter.getSelectors((state) => state.messages);
+
+const modalWindowSelector = modalWindowAdapter.getSelectors((state) => state.modalWindow);
+
+export {
+  channelsSelector,
+  currentChannel,
+  channelsNames,
+  messagesSelector,
+  modalWindowSelector,
+};
