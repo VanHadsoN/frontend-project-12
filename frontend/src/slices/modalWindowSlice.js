47469f2e5ef1a4ffd1ffb65a9d3232c40929ca84
VanHadsoN <107035951+VanHadsoN@users.noreply.github.com>
2023-09-28 12:22:46 +0300

    Step 4. Created "frontend\src\slices\modalWindowSlice.js"

---
+++ frontend/src/slices/modalWindowSlice.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-param-reassign */
+import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+
+const modalWindowAdapter = createEntityAdapter();
+const initialState = modalWindowAdapter.getInitialState({
+  isOpen: false,
+  type: null,
+  relevantChannel: null,
+});
+
+const modalWindowSlice = createSlice({
+  name: 'modalWindow',
+  initialState,
+  reducers: {
+    openModalWindow: (state) => {
+      state.isOpen = true;
+    },
+    closeModalWindow: (state) => {
+      state.isOpen = false;
+    },
+    setCurrentModalType: (state, { payload }) => {
+      state.type = payload;
+    },
+    setRelevantChannel: (state, { payload }) => {
+      state.relevantChannel = payload;
+    },
+  },
+});
+
+export const {
+  openModalWindow,
+  closeModalWindow,
+  setCurrentModalType,
+  setRelevantChannel,
+} = modalWindowSlice.actions;
+export { modalWindowAdapter };
+export default modalWindowSlice.reducer;
