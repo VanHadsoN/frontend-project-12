44fc91a497ec97814fd31bd859f0f8f4bff23c3f
VanHadsoN <107035951+VanHadsoN@users.noreply.github.com>
2023-09-30 13:11:34 +0300

    Step 5. Created "frontend\src\components\chat\chat-panel\ChatPanel.jsx"
    and "frontend\src\components\chat\chat-panel\style.css"

---
+++ frontend/src/components/chat/chat-panel/ChatPanel.jsx
@@ -0,0 +1,22 @@
+import { useTranslation } from 'react-i18next';
+import './style.css';
+
+const ChatPanel = ({ currentChannelName, currentChannelMessagesCount }) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="bg-light mb-4 p-3 shadow-sm small">
+      <p className="m-0">
+        <b className="channel-name">
+          {t('channel.prefix')}
+          {currentChannelName}
+        </b>
+      </p>
+      <span className="message-count">
+        {t('message.messagesCount', { count: currentChannelMessagesCount })}
+      </span>
+    </div>
+  );
+};
+
+export default ChatPanel;
