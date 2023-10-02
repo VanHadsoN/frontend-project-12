aa5d81b5bd70758b4c1c4a011484578947e9a574
VanHadsoN <107035951+VanHadsoN@users.noreply.github.com>
2023-10-02 12:55:23 +0300

    Step 5. Created "frontend\src\validation\messageSchema.js"

---
+++ frontend/src/validation/messageSchema.js
@@ -0,0 +1,10 @@
+import * as Yup from 'yup';
+
+const messageSchema = (requaredField) => Yup.object().shape({
+  text: Yup
+    .string()
+    .trim()
+    .required(requaredField),
+});
+
+export default messageSchema;
