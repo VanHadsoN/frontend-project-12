be8b274b493b5a7b4a92fef21eb643e45013b83e
VanHadsoN <107035951+VanHadsoN@users.noreply.github.com>
2023-10-16 16:24:25 +0300

    Step 9. Created "frontend\src\index.js"

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+import init from './init';
+
+import './index.css';
+
+const app = async () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(await init());
+};
+
+app();
