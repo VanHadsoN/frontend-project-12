f98d41a54320e65264bb0eaea8f7619274e0eb7f
VanHadsoN <107035951+VanHadsoN@users.noreply.github.com>
2023-11-08 14:20:20 +0300

    Fixing hexlet-check. Changed "frontend\src\context\ChatContext.js"

--- frontend/src/context/ChatContext.js
+++ frontend/src/context/ChatContext.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { createContext, useMemo } from 'react';
+import { createContext, useMemo, useCallback } from 'react'; // Import useCallback
 import { useDispatch } from 'react-redux';
 import { addChannel, setCurrentChannel } from '../slices/channelsSlice';
 import { chatContextRoutes } from '../routes';
@@ -10,46 +10,47 @@ const ChatContextProvider = ({ socket, children }) => {
   const dispatch = useDispatch();
   const timeout = 4000;
 
-  const addNewMessage = async (message) => {
+  const addNewMessage = useCallback(async (message) => {
     await socket
       .timeout(timeout)
       .emit('newMessage', message);
-  };
+  }, [socket, timeout]);
 
-  const addNewChannel = async (channel) => {
+  const addNewChannel = useCallback(async (channel) => {
     const { data } = await socket
       .timeout(timeout)
       .emitWithAck('newChannel', channel);
 
     dispatch(addChannel(data));
     dispatch(setCurrentChannel(data.id));
-  };
+  }, [socket, dispatch, timeout]);
 
-  const removeSelectedChannel = async (id) => {
+  const removeSelectedChannel = useCallback(async (id) => {
     await socket
       .timeout(timeout)
       .emit('removeChannel', { id });
-  };
+  }, [socket, timeout]);
 
-  const renameSelectedChannel = async (updateChannel) => {
+  const renameSelectedChannel = useCallback(async (updateChannel) => {
     await socket
       .timeout(timeout)
       .emit('renameChannel', updateChannel);
-  };
+  }, [socket, timeout]);
 
-  const getServerData = async () => {
+  const getServerData = useCallback(async () => {
     const user = JSON.parse(localStorage.getItem('user'));
     const response = await axios.get(chatContextRoutes.data(), { headers: { Authorization: `Bearer ${user.token}` } });
     return response;
-  };
+  }, []);
+
   const contextValue = useMemo(() => ({
     addNewMessage,
     addNewChannel,
     removeSelectedChannel,
     renameSelectedChannel,
     getServerData,
-  }), []);
-  
+  }), [addNewMessage, addNewChannel, removeSelectedChannel, renameSelectedChannel, getServerData]);
+
   return (
     <ChatContext.Provider value={contextValue}>
       {children}
