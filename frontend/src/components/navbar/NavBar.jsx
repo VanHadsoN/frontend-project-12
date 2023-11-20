29c7bc26da491652ec69924ac48870f99f737e93
VanHadsoN <107035951+VanHadsoN@users.noreply.github.com>
2023-11-20 15:57:20 +0300

    Fixing mentor-check. Changed "frontend\src\components\navbar\NavBar.jsx"

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -1,35 +1,33 @@
-import { NavLink, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Navbar, Button, Container } from 'react-bootstrap';
+import { useNavigate, Outlet } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { appRoutes } from '../../routes';
 import { useAuthorization } from '../../hooks';
-import './style.css';
+import { appRoutes } from '../../routes';
 
-const LogoutButton = (handle, title) => {
-  if (localStorage.getItem('user') !== null) {
-    return (
-      <button type="button" className="logout-button" onClick={handle}>{title}</button>
-    );
-  }
-  return null;
-};
 const NavBar = () => {
   const { t } = useTranslation();
-  const { logOut } = useAuthorization();
+  const auth = useAuthorization();
   const navigate = useNavigate();
-
-  const handleLogout = () => {
-    navigate(appRoutes.loginPagePath());
-    logOut();
+  const logOutUser = () => {
+    auth.logOut();
+    navigate(appRoutes.login());
   };
   return (
-    <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-      <div className="nav-container">
-        <NavLink to={appRoutes.chatPagePath()} className="navbar-brand">
-          {t('navigation.chatName')}
-        </NavLink>
-        { LogoutButton(handleLogout, t('navigation.exitBtn')) }
-      </div>
-    </nav>
+    <div className="container-fluid d-flex flex-column vh-100">
+      <Navbar bg="light" expand="lg" className="shadow-sm">
+        <Container>
+          <Navbar.Brand href="/">{t('nav.logo')}</Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end" />
+          {auth.currentUser ? (
+            <Button onClick={logOutUser}>{t('nav.exit')}</Button>
+          ) : (
+            <Button onClick={() => navigate(appRoutes.login())}>{t('nav.enter')}</Button>
+          )}
+        </Container>
+      </Navbar>
+      <Outlet />
+    </div>
   );
 };
 
