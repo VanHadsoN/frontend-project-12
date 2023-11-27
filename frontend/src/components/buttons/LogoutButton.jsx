57ef9fff71562c420dfdb86e726c02fc079db1ca
VanHadsoN <107035951+VanHadsoN@users.noreply.github.com>
2023-11-27 14:05:08 +0300

    Fixing mentor-check. Created "LogoutButton" as a separate component

--- frontend/src/components/buttons/LogoutButton.jsx
+++ frontend/src/components/buttons/LogoutButton.jsx
@@ -1,9 +1,9 @@
 const LogoutButton = (handle, title) => {
-    if (localStorage.getItem('user') !== null) {
-      return (
+  if (localStorage.getItem('user') !== null) {
+    return (
         <button type="button" className="logout-button" onClick={handle}>{title}</button>
-      );
-    }
-    return null;
-  };
+    );
+  }
+  return null;
+};
 export default LogoutButton;
