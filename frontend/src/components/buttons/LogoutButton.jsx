1d4082bcfb371de9baa3143dd9e11e3d95fa3ced
VanHadsoN <107035951+VanHadsoN@users.noreply.github.com>
2023-11-28 13:04:11 +0300

    Fixing mentor-check. Changed "frontend\src\components\buttons\LogoutButton.jsx"

--- frontend/src/components/buttons/LogoutButton.jsx
+++ frontend/src/components/buttons/LogoutButton.jsx
@@ -1,9 +1,16 @@
+import React, { useContext } from 'react';
+import { UserDataContext } from '../../context/UserDataContext';
+
 const LogoutButton = (handle, title) => {
-  if (localStorage.getItem('user') !== null) {
+  const userDataContext = useContext(UserDataContext);
+
+  if (userDataContext.userData !== null) {
     return (
       <button type="button" className="logout-button" onClick={handle}>{title}</button>
     );
   }
+
   return null;
 };
+
 export default LogoutButton;
